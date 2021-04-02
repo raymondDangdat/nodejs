48a7143556e71413dbc0b2183138d84eaa4d6233
Dangdat <raymonddangdat@gmail.com>
2021-04-02 22:03:45 +0100

    HTTP module

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,5 +1,6 @@
 const {readFile, writeFile} = require('fs');
 
+console.log('Start');
 
 readFile('./content/first.txt', 'utf8', (err, result) =>{
     if(err){
@@ -19,7 +20,8 @@ readFile('./content/first.txt', 'utf8', (err, result) =>{
               console.log(err);
               return;  
             }
-            console.log(result);
+            console.log("Done with this task");
         })
     } )
 })
+console.log('Starting next task')
